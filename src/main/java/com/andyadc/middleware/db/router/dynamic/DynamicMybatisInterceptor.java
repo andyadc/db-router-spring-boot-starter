6bc5723fe0e3054a35315c6e29b4570c3b12b8d2
andyadc <andaicheng@gmail.com>
2021-10-14 15:01:43 +0800

    router

---
+++ src/main/java/com/andyadc/middleware/db/router/dynamic/DynamicMybatisInterceptor.java
@@ -0,0 +1,17 @@
+package com.andyadc.middleware.db.router.dynamic;
+
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Invocation;
+
+import java.util.Properties;
+
+/**
+ * Mybatis 拦截器
+ */
+public class DynamicMybatisInterceptor implements Interceptor {
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        return null;
+    }
+}
